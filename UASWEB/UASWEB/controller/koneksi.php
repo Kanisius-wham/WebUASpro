3606dd4d9f2738157f221374de7c9528cd72581b
Kanisius-wham <kanisius.w.im28@gmail.com>
2024-10-31 21:24:01 +0700

    Add files via upload

---
+++ UASWEB/UASWEB/controller/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'webpro';
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Koneksi gagal: {$conn->connect_error}");
+}
